56d05b28dbb10f636450c976ec25b688ff20e877
harryszh <harrys.zh12@gmail.com>
2024-07-08 15:41:42 +0200

    margin pading header footer

--- script.js
+++ script.js
@@ -1,12 +1,16 @@
 function bildWechseln(bildTyp) {
-    var bild = document.getElementById('emojis').getElementsByTagName('img')[0];
+    var bild = document.getElementById('emoji-img');
     if (bildTyp === 'gut') {
         bild.src = "images/glücklich.svg";
         bild.alt = "Glückliches Bild";
-    } 
-    
-    else if (bildTyp === 'schlecht') {
+    } else if (bildTyp === 'schlecht') {
         bild.src = "images/traurig.svg";
         bild.alt = "Trauriges Bild";
     }
+}
+
+function resetPage() {
+    var bild = document.getElementById('emoji-img');
+    bild.src = "images/neutral.svg";
+    bild.alt = "neutral";
 }
